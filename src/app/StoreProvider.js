89cd90aab39b4d6a3238b470426ca4c0e38c6dad
Adil <adildevmate@gmail.com>
2025-12-17 14:12:29 +0500

    dd

--- src/app/StoreProvider.js
+++ src/app/StoreProvider.js
@@ -3,8 +3,6 @@
 import { useRef, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { Store } from '@/lib/redux/store';
-import { onAuthStateChange } from '@/lib/firebase/auth/auth';
-import { getUser } from '@/lib/firebase/firestore/user';
 import { setUser, clearUser, setLoading } from '@/lib/redux/features/ActiveUserSlice/ActiveUserSlice';
 
 /**
@@ -25,36 +23,6 @@ export default function StoreProvider({ children }) {
   useEffect(() => {
     const store = storeRef.current;
     if (!store) return;
-
-    // Set initial loading state
-    store.dispatch(setLoading(true));
-
-    // Set up auth state listener
-    const unsubscribe = onAuthStateChange(async (user) => {
-      try {
-        if (user) {
-          // User is signed in
-          const userInfo = await getUser(user.uid);
-          store.dispatch(setUser({
-            user: user.toJSON(),
-            userInfo: userInfo || {},
-          }));
-        } else {
-          // User is signed out
-          store.dispatch(clearUser());
-        }
-      } catch (error) {
-        console.error('Error handling auth state change:', error);
-        store.dispatch(clearUser());
-      }
-    });
-
-    // Cleanup subscription on unmount
-    return () => {
-      if (unsubscribe) {
-        unsubscribe();
-      }
-    };
   }, []);
 
   return <Provider store={storeRef.current}>{children}</Provider>;
